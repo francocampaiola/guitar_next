162013ab4bd682b855a05768e5c14fd2b3ed980a
Franco Campaiola <francocampaiola@hotmail.com>
2022-02-03 00:21:22 -0300

    :+1: Se incorpora al archivo next.config.js el dominio de Cloudinary como de confianza.

--- components/Entrada.js
+++ components/Entrada.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { formatearFecha } from '../helpers'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Entrada = ({entrada}) => {
     const {titulo, resumen, imagen, published_at, id } = entrada;
 
   return (
     <article>
+        <Image
+            src={imagen.url}
+            alt={`imagen blog ${titulo}`}
+            width={800}
+            height={600}
+            layout='responsive'
+        />
         <div>
             <h1>{titulo}</h1>
             <p>{formatearFecha(published_at)}</p>
