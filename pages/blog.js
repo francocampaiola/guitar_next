076eb9bfc8b38ecd90d0f124a3075aee1e331ed7
Franco Campaiola <francocampaiola@hotmail.com>
2022-02-03 18:59:39 -0300

    :+1: Se incorporan funciones de SSP y SP para el render de las entradas de blog a partir de la API

--- pages/blog.js
+++ pages/blog.js
@@ -27,7 +27,7 @@ const Blog = ({entradas}) => {
 
 export async function getStaticProps() {
 
-    const url = 'http://localhost:1337/blogs'
+    const url = `${process.env.API_URL}/blogs`
     const respuesta = await fetch(url)
     const entradas = await respuesta.json();
 
