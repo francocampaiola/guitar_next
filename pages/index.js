9993bab48d0c04cb9e9a9e54ba61671d7c0a665e
Franco Campaiola <francocampaiola@hotmail.com>
2022-02-10 19:55:00 -0300

    :+1: Se incorpora un nuevo componente y se suma al Inicio del sitio web

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Layout from '../components/Layout';
 import Listado from '../components/Listado';
 import Curso from '../components/Curso';
+import ListadoBlog from '../components/ListadoBlog';
 
-export default function Home({guitarras, cursos}) {
+export default function Home({guitarras, cursos, entradas}) {
   return (
       <Layout
         pagina="Inicio"
@@ -17,6 +18,12 @@ export default function Home({guitarras, cursos}) {
         <Curso
           cursos={cursos}
         />
+
+        <section className='contenedor'>
+          <ListadoBlog 
+            entradas={entradas}
+          />
+        </section>
       </Layout>
   )
 }
@@ -24,21 +31,25 @@ export default function Home({guitarras, cursos}) {
 export async function getServerSideProps() {
   const urlGuitarras = `${process.env.API_URL}/guitarras`
   const urlCursos = `${process.env.API_URL}/cursos`
+  const urlBlog = `${process.env.API_URL}/blogs?_limit=3&_sort=created_at:desc`
 
-  const [resGuitarras, resCursos] = await Promise.all([
+  const [resGuitarras, resCursos, resBlogs] = await Promise.all([
       fetch(urlGuitarras),
-      fetch(urlCursos)
+      fetch(urlCursos),
+      fetch(urlBlog)
   ])
 
-  const [guitarras, cursos] = await Promise.all([
+  const [guitarras, cursos, entradas] = await Promise.all([
     resGuitarras.json(),
-    resCursos.json()
+    resCursos.json(),
+    resBlogs.json()
   ])
   
   return {
       props: {
           guitarras,
-          cursos
+          cursos,
+          entradas
       }
   }
 }
